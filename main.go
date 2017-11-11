f5edb36460ec3e0cb03a5a4a05b4b0c19b0673fa
xmxiaoq <419566089@qq.com>
2017-11-11 18:44:38 +0800

    add tcp server

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"github.com/xmxiaoq/golog"
 	"github.com/yuin/gopher-lua"
 	"go.uber.org/zap"
+	"net"
+	"os"
+	"strconv"
 )
 
 var logger *zap.Logger
@@ -37,6 +40,50 @@ func DoGoLua() {
 	}
 }
 
+func Tcp() {
+	const (
+		CONN_HOST = "localhost"
+		CONN_PORT = "8888"
+		CONN_TYPE = "tcp"
+	)
+
+	// Listen for incoming connections.
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		fmt.Println("new connetion")
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+}
+
+// Handles incoming requests.
+func handleRequest(conn net.Conn) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	reqLen, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	// Send a response back to person contacting us.
+	conn.Write([]byte(strconv.Itoa(reqLen) + " Message received."))
+	// Close the connection when you're done with it.
+	conn.Close()
+}
+
 func main() {
 	logger = golog.Logger
 	sugar = golog.Sugar
@@ -52,14 +99,15 @@ func main() {
 	fs := gofs.OsFs
 	fs.Name()
 
-	for {
-		var s string
-		fmt.Scan(&s)
-		if s == "c" {
-			break
-		}
-	}
+	//for {
+	//	var s string
+	//	fmt.Scan(&s)
+	//	if s == "c" {
+	//		break
+	//	}
+	//}
 
 	//DoGopherLua()
-	DoGoLua()
+	//DoGoLua()
+	Tcp()
 }
